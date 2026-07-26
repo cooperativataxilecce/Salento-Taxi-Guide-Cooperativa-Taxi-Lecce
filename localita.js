c84cb3bd89276a15f177e7546cf92df507ba0d56
cooperativataxilecce <cooperativataxilecce@gmail.com>
2026-07-27 01:04:23 +0200

    Create localita.js

---
+++ localita.js
@@ -0,0 +1,125 @@
+const localita = [
+
+{
+    nome: "Lecce",
+
+    categorie: [
+        "cultura",
+        "borghi",
+        "enogastronomia"
+    ],
+
+    zona: "centro",
+
+    durata: 4,
+
+    stagioni: [
+        "primavera",
+        "estate",
+        "autunno",
+        "inverno"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia",
+        "amici",
+        "solo"
+    ]
+},
+
+{
+    nome: "Otranto",
+
+    categorie: [
+        "mare",
+        "panorami",
+        "cultura"
+    ],
+
+    zona: "adriatico",
+
+    durata: 5,
+
+    stagioni: [
+        "primavera",
+        "estate",
+        "autunno"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia",
+        "amici"
+    ]
+},
+
+{
+    nome: "Porto Badisco",
+
+    categorie: [
+        "mare",
+        "panorami",
+        "relax"
+    ],
+
+    zona: "adriatico",
+
+    durata: 3,
+
+    stagioni: [
+        "estate"
+    ],
+
+    idealePer: [
+        "coppia"
+    ]
+},
+
+{
+    nome: "Castro",
+
+    categorie: [
+        "mare",
+        "panorami"
+    ],
+
+    zona: "adriatico",
+
+    durata: 4,
+
+    stagioni: [
+        "primavera",
+        "estate"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia"
+    ]
+},
+
+{
+    nome: "Santa Cesarea Terme",
+
+    categorie: [
+        "mare",
+        "relax",
+        "panorami"
+    ],
+
+    zona: "adriatico",
+
+    durata: 4,
+
+    stagioni: [
+        "primavera",
+        "estate"
+    ],
+
+    idealePer: [
+        "coppia"
+    ]
+}
+
+];
