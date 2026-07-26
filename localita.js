7a339816b805c5dcc8e934b7d9552fcba9905f65
cooperativataxilecce <cooperativataxilecce@gmail.com>
2026-07-27 01:34:04 +0200

    Update localita.js

--- localita.js
+++ localita.js
@@ -292,4 +292,315 @@ const localita = [
     ]
 }
 
+    {
+    nome: "San Foca",
+
+    zona: "adriatico",
+
+    categorie: [
+        "mare",
+        "relax",
+        "famiglia"
+    ],
+
+    idealePer: [
+        "famiglia",
+        "coppia"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 4,
+
+    priorita: 8,
+
+    vicine: [
+        "Torre dell'Orso",
+        "Frigole"
+    ]
+},
+
+
+{
+    nome: "Santa Cesarea Terme",
+
+    zona: "adriatico",
+
+    categorie: [
+        "mare",
+        "relax",
+        "panorami"
+    ],
+
+    idealePer: [
+        "coppia"
+    ],
+
+    stagioni: [
+        "primavera",
+        "estate"
+    ],
+
+    durata: 4,
+
+    priorita: 8,
+
+    vicine: [
+        "Castro",
+        "Porto Badisco"
+    ]
+},
+
+
+{
+    nome: "Porto Badisco",
+
+    zona: "adriatico",
+
+    categorie: [
+        "mare",
+        "natura",
+        "panorami",
+        "relax"
+    ],
+
+    idealePer: [
+        "coppia"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 3,
+
+    priorita: 8,
+
+    vicine: [
+        "Otranto",
+        "Castro"
+    ]
+},
+
+
+{
+    nome: "Torre San Giovanni",
+
+    zona: "ionio",
+
+    categorie: [
+        "mare",
+        "relax",
+        "movida"
+    ],
+
+    idealePer: [
+        "famiglia",
+        "amici",
+        "coppia"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 5,
+
+    priorita: 8,
+
+    vicine: [
+        "Pescoluse",
+        "Gallipoli"
+    ]
+},
+
+
+{
+    nome: "Baia Verde",
+
+    zona: "ionio",
+
+    categorie: [
+        "mare",
+        "movida"
+    ],
+
+    idealePer: [
+        "amici",
+        "giovani"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 5,
+
+    priorita: 8,
+
+    vicine: [
+        "Gallipoli",
+        "Rivabella"
+    ]
+},
+
+
+{
+    nome: "Rivabella",
+
+    zona: "ionio",
+
+    categorie: [
+        "mare",
+        "famiglia",
+        "relax"
+    ],
+
+    idealePer: [
+        "famiglia",
+        "coppia"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 4,
+
+    priorita: 7,
+
+    vicine: [
+        "Gallipoli"
+    ]
+},
+
+
+{
+    nome: "Punta Prosciutto",
+
+    zona: "ionio",
+
+    categorie: [
+        "mare",
+        "natura",
+        "relax"
+    ],
+
+    idealePer: [
+        "famiglia",
+        "coppia"
+    ],
+
+    stagioni: [
+        "estate"
+    ],
+
+    durata: 5,
+
+    priorita: 9,
+
+    vicine: [
+        "Porto Cesareo",
+        "Torre Lapillo"
+    ]
+},
+
+
+{
+    nome: "Acaya",
+
+    zona: "entroterra",
+
+    categorie: [
+        "borghi",
+        "cultura",
+        "storia"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia"
+    ],
+
+    stagioni: [
+        "tutto l'anno"
+    ],
+
+    durata: 3,
+
+    priorita: 7,
+
+    vicine: [
+        "Lecce"
+    ]
+},
+
+
+{
+    nome: "Galatina",
+
+    zona: "entroterra",
+
+    categorie: [
+        "borghi",
+        "cultura",
+        "tradizioni",
+        "enogastronomia"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia"
+    ],
+
+    stagioni: [
+        "tutto l'anno"
+    ],
+
+    durata: 4,
+
+    priorita: 8,
+
+    vicine: [
+        "Lecce",
+        "Martano"
+    ]
+},
+
+
+{
+    nome: "Martano",
+
+    zona: "entroterra",
+
+    categorie: [
+        "borghi",
+        "cultura",
+        "tradizioni",
+        "enogastronomia"
+    ],
+
+    idealePer: [
+        "coppia",
+        "famiglia"
+    ],
+
+    stagioni: [
+        "tutto l'anno"
+    ],
+
+    durata: 3,
+
+    priorita: 7,
+
+    vicine: [
+        "Galatina",
+        "Otranto"
+    ]
+
+    
+}
+
 ];
