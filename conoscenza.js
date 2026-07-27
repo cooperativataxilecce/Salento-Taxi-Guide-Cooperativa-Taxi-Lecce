75813ae327b9964f74adc692ec4dde3c806a72ca
cooperativataxilecce <cooperativataxilecce@gmail.com>
2026-07-28 01:08:45 +0200

    Create conoscenza.js

---
+++ conoscenza.js
@@ -0,0 +1,214 @@
+const informazioniSalento = {
+
+
+"Lecce": {
+
+descrizione:
+"Capoluogo del Salento, famosa per il barocco leccese, le piazze storiche e la cucina tradizionale.",
+
+fare:[
+"Visitare Piazza Duomo",
+"Vedere la Basilica di Santa Croce",
+"Passeggiare nel centro storico",
+"Visitare l'Anfiteatro Romano",
+"Provare pasticciotto e prodotti tipici salentini",
+"Visitare il Castello Carlo V"
+],
+
+vedere:[
+"Piazza Sant'Oronzo",
+"Porta Napoli",
+"Piazza Duomo",
+"Basilica di Santa Croce",
+"Anfiteatro Romano"
+],
+
+consigliatoPer:[
+"coppie",
+"famiglie",
+"amanti della cultura"
+]
+
+},
+
+
+
+"Otranto": {
+
+descrizione:
+"Città sul mare Adriatico con un centro storico ricco di storia, spiagge e panorami spettacolari.",
+
+fare:[
+"Visitare la Cattedrale con il mosaico dell'Albero della Vita",
+"Visitare il Castello Aragonese",
+"Passeggiare sul lungomare",
+"Raggiungere la Baia dei Turchi",
+"Vedere il tramonto sul mare",
+"Visitare il Lago di Bauxite"
+],
+
+vedere:[
+"Cattedrale di Otranto",
+"Castello Aragonese",
+"Lago di Bauxite",
+"Centro storico",
+"Porto di Otranto"
+],
+
+consigliatoPer:[
+"coppie",
+"famiglie",
+"mare e cultura"
+]
+
+},
+
+
+
+"Gallipoli": {
+
+descrizione:
+"Città sullo Ionio famosa per il borgo antico sull'isola, le spiagge e la vita serale.",
+
+fare:[
+"Visitare il centro storico",
+"Passeggiare sul ponte della città vecchia",
+"Visitare il Castello di Gallipoli",
+"Andare nelle spiagge di Baia Verde",
+"Guardare il tramonto dal porto",
+"Vivere la movida estiva"
+],
+
+vedere:[
+"Centro storico",
+"Castello di Gallipoli",
+"Cattedrale di Sant'Agata",
+"Fontana Greca",
+"Porto"
+],
+
+consigliatoPer:[
+"giovani",
+"coppie",
+"amici"
+]
+
+},
+
+
+
+"Santa Maria di Leuca": {
+
+descrizione:
+"Il punto più a sud del Salento dove si incontrano Mar Ionio e Mar Adriatico.",
+
+fare:[
+"Visitare il Santuario di Santa Maria de Finibus Terrae",
+"Fare un giro in barca nelle grotte marine",
+"Passeggiare sul lungomare",
+"Vedere il tramonto dal promontorio",
+"Visitare Punta Ristola"
+],
+
+vedere:[
+"Faro di Leuca",
+"Santuario",
+"Punta Ristola",
+"Grotte marine"
+],
+
+consigliatoPer:[
+"coppie",
+"famiglie",
+"amanti del mare"
+]
+
+},
+
+
+
+"Porto Cesareo": {
+
+descrizione:
+"Località ionica famosa per spiagge sabbiose, mare cristallino e aree naturali.",
+
+fare:[
+"Andare nelle spiagge",
+"Visitare l'Area Marina Protetta",
+"Fare escursioni in barca",
+"Visitare Torre Cesarea",
+"Rilassarsi sul lungomare"
+],
+
+vedere:[
+"Torre Cesarea",
+"Isola dei Conigli",
+"Area Marina Protetta",
+"Spiagge di Porto Cesareo"
+],
+
+consigliatoPer:[
+"famiglie",
+"coppie",
+"amanti del mare"
+]
+
+},
+
+
+
+"Torre dell'Orso": {
+
+descrizione:
+"Famosa località dell'Adriatico con spiagge, mare limpido e panorami naturali.",
+
+fare:[
+"Andare sulla spiaggia di Torre dell'Orso",
+"Vedere le Due Sorelle",
+"Visitare la Grotta della Poesia",
+"Fare passeggiate sul lungomare"
+],
+
+vedere:[
+"Due Sorelle",
+"Grotta della Poesia",
+"Baia di Torre dell'Orso"
+],
+
+consigliatoPer:[
+"famiglie",
+"coppie",
+"mare e natura"
+]
+
+},
+
+
+
+"Pescoluse": {
+
+descrizione:
+"Conosciuta come le Maldive del Salento per la sabbia chiara e il mare trasparente.",
+
+fare:[
+"Trascorrere una giornata in spiaggia",
+"Fare il bagno nelle acque cristalline",
+"Vedere il tramonto sul mare",
+"Rilassarsi nei lidi della zona"
+],
+
+vedere:[
+"Le spiagge di Pescoluse",
+"Litorale delle Maldive del Salento"
+],
+
+consigliatoPer:[
+"famiglie",
+"coppie",
+"relax"
+]
+
+}
+
+
+};
